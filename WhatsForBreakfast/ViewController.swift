be64b8c72bd02fcd991497e353c13dafd70adcd1
Matthew Poelsterl <mpoelsterl1217@gmail.com>
2021-07-20 10:16:37 -0500

    Finished MVP
    New image of a breakfast appears on button press

--- WhatsForBreakfast/ViewController.swift
+++ WhatsForBreakfast/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Globals
+    var randomNum: Int = 0
+    func newBreakfastImage() {
+        randomNum = Int.random(in: 1...4)
+        switch randomNum {
+        case 0:
+            breakfastImageView.image = UIImage(named: "Bagel")
+        case 1:
+            breakfastImageView.image = UIImage(named: "Cereal")
+        case 2:
+            breakfastImageView.image = UIImage(named: "EggsBacon")
+        case 3:
+            breakfastImageView.image = UIImage(named: "Pancakes")
+        case 4:
+            breakfastImageView.image = UIImage(named: "Toast")
+        default:
+            breakfastImageView.image = UIImage(named: "AskAgain")
+        }
+    }
+    
+    //Outlets
+    @IBOutlet weak var breakfastImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    //Actions
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        newBreakfastImage()
+    }
+    
 }
 
